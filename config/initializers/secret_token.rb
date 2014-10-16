4dc4f625ebaefa292320599ec2980784e6ada92b
Ken Rettberg <krettberg@teachtci.com>
2014-10-15 20:39:40 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportsband::Application.config.secret_key_base = '67ea4ddd551fb0d75d52c7434887c92d2147721cd85d82a7af033dd6b9d0da1735853a432b51de694f113fced314f9b7d9ac9cfe0ed618e856681ac166bd0eb2'
